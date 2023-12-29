0bb48f27ccdd5d01e87ee7b07f1bfc019d8194b7
g4nd4lf <rafaelromerovicente@gmail.com>
2023-12-29 21:14:01 +0100

    14_2 ok

--- 2023/day14/day14_2c.py
+++ 2023/day14/day14_2c.py
@@ -4,8 +4,8 @@ from typing import Callable, cast
 import os
 
 os.chdir("./day14")
-input1='sample.txt'
-#input1='input.txt'
+#input1='sample.txt'
+input1='input.txt'
 
 GridPoint = tuple[int, int]
 Grid = dict[GridPoint, str]
@@ -59,45 +59,48 @@ def _roll(
             rocks.remove(rock)
             rocks.add(cast(GridPoint, new_rock))
 
+def roll_down(rocks, walls, height):
+    for row, col in sorted(rocks, reverse=True):
+        new_row = None
 
-def roll_up2(rocks: Points, walls: Points) -> None:
-    _roll(
-        rocks,
-        walls,
-        reverse_sort=False,
-        range_builder=lambda row: range(row - 1, -1, -1),
-        index_to_replace=0,
-    )
+        for potential_new_row in range(row +1, height):
+            potential_new_spot = potential_new_row, col
+            if potential_new_spot in walls or potential_new_spot in rocks:
+                break
+            new_row = potential_new_row
 
+        if new_row is not None:
+            rocks.remove((row, col))
+            rocks.add((new_row, col))
 
-def roll_down(rocks: Points, walls: Points, grid_size: int) -> None:
-    _roll(
-        rocks,
-        walls,
-        reverse_sort=True,
-        range_builder=lambda row: range(row + 1, grid_size),
-        index_to_replace=0,
-    )
+def roll_left(rocks, walls):
+    for row, col in sorted(rocks,key=lambda loc: loc[1]): #ordeno por columnas en vez de filas
+        new_col = None
 
+        for potential_new_col in range(col - 1, -1, -1):
+            potential_new_spot = row, potential_new_col
+            if potential_new_spot in walls or potential_new_spot in rocks:
+                break
+            new_col = potential_new_col
 
-def roll_left(rocks: Points, walls: Points) -> None:
-    _roll(
-        rocks,
-        walls,
-        reverse_sort=False,
-        range_builder=lambda col: range(col - 1, -1, -1),
-        index_to_replace=1,
-    )
+        if new_col is not None:
+            rocks.remove((row, col))
+            rocks.add((row, new_col))    
 
+def roll_right(rocks, walls, width):
+   for row, col in sorted(rocks,key=lambda loc: loc[1],reverse=True): #ordeno por columnas en vez de filas
+        new_col = None
 
-def roll_right(rocks: Points, walls: Points, grid_size: int) -> None:
-    _roll(
-        rocks,
-        walls,
-        reverse_sort=True,
-        range_builder=lambda col: range(col + 1, grid_size),
-        index_to_replace=1,
-    )
+        for potential_new_col in range(col + 1, width):
+            potential_new_spot = row, potential_new_col
+            if potential_new_spot in walls or potential_new_spot in rocks:
+                break
+            new_col = potential_new_col
+
+        if new_col is not None:
+            rocks.remove((row, col))
+            rocks.add((row, new_col))  
+
 def roll_up(rocks,walls):
     for row, col in sorted(rocks):
         new_row = None
@@ -112,7 +115,7 @@ def roll_up(rocks,walls):
             rocks.remove((row, col))
             rocks.add((new_row, col))
 
-def part_1(rocks,walls,height) -> int:
+def part_1(rocks,walls,height):
     roll_up(rocks, walls)
     return sum(height - row for row, _ in rocks)
 
@@ -129,29 +132,33 @@ with open(input1) as f:
 res=part_1(rocks,walls,len(lines))
 
 print("RESULT: ", res)
-def part_2(self) -> int:
-    rocks = set(parse_grid(self.input, ignore_chars=".#"))
-    walls = set(parse_grid(self.input, ignore_chars=".O"))
-    grid_size = len(self.input)
+
+
+def part_2(rocks,walls,height,width):
     NUM_CYCLES = 1_000_000_000
 
-    states: dict[frozenset[GridPoint], int] = {}
-
+    states={}
     i = 0
     while i < NUM_CYCLES:
         roll_up(rocks, walls)
         roll_left(rocks, walls)
-        roll_down(rocks, walls, grid_size)
-        roll_right(rocks, walls, grid_size)
+        roll_down(rocks, walls, height)
+        roll_right(rocks, walls, width)
 
-        state = frozenset(rocks)
+        state = frozenset(rocks) #Tras cada ciclo up,left,down,right genero un diccionario con el estado de las rocas
 
-        if state in states and i < 500:
-            distance_to_goal = NUM_CYCLES - i
-            loop_length = i - states[state]
-            i = NUM_CYCLES - distance_to_goal % loop_length
+        if state in states and i < 500:    #Si antes de 500 iteracciones encuentro que el nuevo diccionario ya existe en el diccionario de diccionarios states, es que se repite el ciclo
+            distance_to_goal = NUM_CYCLES - i  #Esto es el numero de iteracciones para llegar al final
+            loop_length = i - states[state]   #Esto es el numero de iteracciones que tarda en repetirse el ciclo
+            i = NUM_CYCLES - distance_to_goal % loop_length #si p.ej el numero de ciclos se 20, y quedan 16 para llegar al final y el ciclo se repite cada 3 veces
+                                                            #i=20-16%3 = 20-1=19...porque estaba en i=4...7...10...13...16...19 son puntos en que coincide el estado ya calculado, 
+                                                            #así que solo tenemos que continuar desde el estado 19
 
-        states[state] = i
+        states[state] = i   #al final almaceno en el diccionaro states el nuevo diccionaro de rocas resultantes state
         i += 1
 
-    return sum(grid_size - row for row, _ in rocks)
+    return sum(height - row for row, _ in rocks)
+
+res2=part_2(rocks,walls,len(lines),len(lines[0])-1)
+
+print("RESULT2: ", res2)
